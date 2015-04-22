4c3282fb6dab531312ab61ac1d3c1992893aa883
Matt Mongeon <matthewmongeon2015@u.northwestern.edu>
2015-04-21 21:59:55 -0500

    Initial commit

---
+++ hw6/firmware_me433/blinky_leds.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,20 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# pic32mx_250f128b configuration
+CND_ARTIFACT_DIR_pic32mx_250f128b=dist/pic32mx_250f128b/production
+CND_ARTIFACT_NAME_pic32mx_250f128b=blinky_leds.X.production.hex
+CND_ARTIFACT_PATH_pic32mx_250f128b=dist/pic32mx_250f128b/production/blinky_leds.X.production.hex
+CND_PACKAGE_DIR_pic32mx_250f128b=${CND_DISTDIR}/pic32mx_250f128b/package
+CND_PACKAGE_NAME_pic32mx_250f128b=blinkyleds.x.tar
+CND_PACKAGE_PATH_pic32mx_250f128b=${CND_DISTDIR}/pic32mx_250f128b/package/blinkyleds.x.tar
+# pic32mz_ec_sk configuration
+CND_ARTIFACT_DIR_pic32mz_ec_sk=dist/pic32mz_ec_sk/production
+CND_ARTIFACT_NAME_pic32mz_ec_sk=blinky_leds.X.production.hex
+CND_ARTIFACT_PATH_pic32mz_ec_sk=dist/pic32mz_ec_sk/production/blinky_leds.X.production.hex
+CND_PACKAGE_DIR_pic32mz_ec_sk=${CND_DISTDIR}/pic32mz_ec_sk/package
+CND_PACKAGE_NAME_pic32mz_ec_sk=blinkyleds.x.tar
+CND_PACKAGE_PATH_pic32mz_ec_sk=${CND_DISTDIR}/pic32mz_ec_sk/package/blinkyleds.x.tar
